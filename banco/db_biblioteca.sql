5c16e2cdf4a08004f454e235084354d4449bab5c
Crocs <138597970+CHcrocs@users.noreply.github.com>
2025-06-13 00:37:59 -0300

    banco update

--- banco/db_biblioteca.sql
+++ banco/db_biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/06/2025 às 04:05
+-- Tempo de geração: 13/06/2025 às 05:37
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -34,14 +34,6 @@ CREATE TABLE `tb_livros` (
   `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `tb_livros`
---
-
-INSERT INTO `tb_livros` (`id_livro`, `titulo`, `autor`, `id_usuario`) VALUES
-(1, 'diario de um banana 1', 'joca minhoca', 2),
-(2, 'diario de um banana 1', 'joca minhoca', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -56,17 +48,6 @@ CREATE TABLE `tb_usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `tb_usuarios`
---
-
-INSERT INTO `tb_usuarios` (`id`, `nome`, `contato`, `senha`) VALUES
-(1, 'vitor', '1111111111111111111111111', '$2y$10$1DMpvhIBpH83gKKCyRm1JOd'),
-(2, 'vitor2', '111', '111'),
-(3, 'vitor3', '999999999999999', '666'),
-(4, 'carlos', '14124124124224', '666'),
-(5, 'vitor94568345968', '25127590128590128591289', '666');
-
---
 -- Índices para tabelas despejadas
 --
 
@@ -74,8 +55,7 @@ INSERT INTO `tb_usuarios` (`id`, `nome`, `contato`, `senha`) VALUES
 -- Índices de tabela `tb_livros`
 --
 ALTER TABLE `tb_livros`
-  ADD PRIMARY KEY (`id_livro`),
-  ADD KEY `livro_usuario` (`id_usuario`);
+  ADD PRIMARY KEY (`id_livro`);
 
 --
 -- Índices de tabela `tb_usuarios`
@@ -91,23 +71,13 @@ ALTER TABLE `tb_usuarios`
 -- AUTO_INCREMENT de tabela `tb_livros`
 --
 ALTER TABLE `tb_livros`
-  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_livro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de tabela `tb_usuarios`
 --
 ALTER TABLE `tb_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `tb_livros`
---
-ALTER TABLE `tb_livros`
-  ADD CONSTRAINT `livro_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id`) ON DELETE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
